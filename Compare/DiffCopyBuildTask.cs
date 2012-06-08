a577db9dc8e321e7c9b250d5af173fa2945e38ee
Joshua Gall <josh@imtraum.com>
2012-06-07 21:35:53 -0500

    Working on a Rough Implementation of the Built Task
    This commit is a work in progress.

---
+++ Compare/DiffCopyBuildTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Compare
+{
+    public class DiffCopyBuildTask : Task
+    {
+        public DiffCopyBuildTask() { }
+
+        [Required]
+        public string SourceDirectory { get; set; }
+        [Required]
+        public string DestinationDirectory { get; set; }
+
+        [Output]
+        public ITaskItem[] NewFiles { get; set; }
+        [Output]
+        public ITaskItem[] ModifiedFiles { get; set; }
+        [Output]
+        public ITaskItem[] NotInSourceFiles { get; set; }
+
+        public override bool Execute()
+        {
+            if (!Directory.Exists(this.SourceDirectory))
+            {
+                // can't run if the directory doesn't exist
+                base.Log.LogError("The source directory specified does not exist.");
+                return false;
+            }
+            if (!Directory.Exists(this.DestinationDirectory))
+            {
+                // can't run if the directory doesn't exist
+                base.Log.LogError("The destination directory specified does not exist.");
+                return false;
+            }
+
+            base.Log.LogMessage(MessageImportance.Normal, "Comparing {0} to {1}", this.SourceDirectory, this.DestinationDirectory);
+
+            var comparer = new ByteStreamComparer();
+            var result = comparer.Compare(this.SourceDirectory, this.DestinationDirectory);
+
+            this.HandleNewFiles(result);
+            this.HandleModifiedFiles(result);
+            this.HandledNotInSourceFiles(result);
+
+            return true;
+        }
+
+        private void HandleNewFiles(ComparisonResult result)
+        {
+            var newFiles = new List<ITaskItem>();
+            base.Log.LogMessage(MessageImportance.High, "New Files", null);
+            foreach (var file in result.NewFiles)
+            {
+                base.Log.LogMessage(MessageImportance.Normal, "\t{0}", file);
+                newFiles.Add(new TaskItem(file));
+            }
+            this.NewFiles = newFiles.ToArray();
+        }
+
+        private void HandleModifiedFiles(ComparisonResult result)
+        {
+            var modifiedFiles = new List<ITaskItem>();
+            base.Log.LogMessage(MessageImportance.High, "Modified Files", null);
+            foreach (var file in result.ModifiedFiles)
+            {
+                base.Log.LogMessage(MessageImportance.Normal, "\t{0}", file);
+                modifiedFiles.Add(new TaskItem(file));
+            }
+            this.ModifiedFiles = modifiedFiles.ToArray();
+        }
+
+        private void HandledNotInSourceFiles(ComparisonResult result)
+        {
+            var notInSourceFiles = new List<ITaskItem>();
+            base.Log.LogMessage(MessageImportance.High, "Deleted Files", null);
+            foreach (var file in result.NotInSource)
+            {
+                base.Log.LogMessage(MessageImportance.Normal, "\t{0}", file);
+                notInSourceFiles.Add(new TaskItem(file));
+            }
+            this.NotInSourceFiles = notInSourceFiles.ToArray();
+        }
+    }
+}
